c0e75fe3a6c29e6589b178b80583640be95e2e69
Marius <mariusmihalache08@gmail.com>
2025-01-18 20:38:58 +0200

    Pagina Login

---
+++ src/test/java/SearchButton.java
@@ -0,0 +1,25 @@
+import org.automation.framework.BrowserManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SearchButton {
+    BrowserManager manager = new BrowserManager();
+
+    @Test
+    public void testSearchButton() throws InterruptedException {
+        manager.openBrowser();
+        WebDriver driver = manager.getDriver();
+        driver.get("https://www.foxfishing.ro");
+
+        // XPath for the button
+        String buttonXPath = "//*[@id=\"products_search\"]/div[2]/div/div[4]/div/div/div/div[2]/div[2]/div/form/button";
+        driver.findElement(By.xpath(buttonXPath)).click();
+
+        // Wait for 10 seconds (you might want to use WebDriverWait instead of Thread.sleep)
+        Thread.sleep(10000);
+
+        // Close the browser after the test
+        driver.quit();
+    }
+}
--- src/test/java/YouTubePage.java
+++ src/test/java/YouTubePage.java
@@ -10,36 +10,32 @@ public class YouTubePage {
     BrowserManager manager = new BrowserManager();
 
     @Test
-    public void YouTube() throws InterruptedException {
+    public void testYouTube() throws InterruptedException {
         manager.openBrowser();
-
         WebDriver driver = manager.getDriver();
         driver.get("https://www.foxfishing.ro");
 
-        //  WebDriverWait wait = new WebDriverWait(driver, 10); // Așteaptă până la 10 secunde
-        //   WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/form[2]/div/div/button/span")))element.click();
+        // Așteaptă până la 10 secunde pentru ca elementul să fie prezent
+      //  WebDriverWait wait = new WebDriverWait(driver, 10);
+       // WebElement acceptAllButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"yDmH0d\"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/form[2]/div/div/button/span")));
+     //   acceptAllButton.click();
 
+        // Face clic pe iconiță
+        String iconXPath = "//*[@id=\"index_index\"]/div[1]/div/div[1]/div/div/div/div[2]/div[2]/div/div/a[3]/i";
+        driver.findElement(By.xpath(iconXPath)).click();
 
-        String icon = "//*[@id=\"index_index\"]/div[1]/div/div[1]/div/div/div/div[2]/div[2]/div/div/a[3]/i";
-        driver.findElement(By.xpath(icon)).click();
-
+        // Așteaptă 10 secunde (ideal să folosești WebDriverWait)
         Thread.sleep(10000);
 
+        // Verifică dacă elementul este vizibil și activ înainte de a face clic
+        WebElement checkElement = driver.findElement(By.xpath("//*[@id=\"yDmH0d\"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/form[2]/div/div"));
+        if (checkElement.isDisplayed() && checkElement.isEnabled()) {
+            checkElement.click();
+        } else {
+            System.out.println("Elementul nu este vizibil pe pagină");
+        }
 
-        //   driver.findElement(By.xpath("//*[@id=\"yDmH0d\"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/form[1]/div/div/button/span")).click();
-        //  driver.findElement(By.xpath(icon)).click();
-        //  Thread.sleep(10000);
-
-        // nu gaseste elementul in pagina....in cazul de fata butonul de accept all
-        // numerge nici cu reject all
-
-        // WebElement element = driver.findElement(By.xpath("//*[@id=\"yDmH0d\"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/form[2]/div/div/button/span"));
-        //  if (element.isDisplayed() && element.isEnabled()) {
-        //     element.click();
-        //   } else {
-        //     System.out.println("Elementul nu este vizibil pe pagina");
-        //  }
-
+        // Închide browserul după test
+        driver.quit();
     }
 }
-
