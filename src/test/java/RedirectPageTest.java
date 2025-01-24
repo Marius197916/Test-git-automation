9d0c4d554b357043e2ccc814f19bacbf1ceccf80
Marius <mariusmihalache08@gmail.com>
2025-01-24 09:53:34 +0200

    redirectpagetest1

--- src/test/java/RedirectPageTest.java
+++ src/test/java/RedirectPageTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class RedirectPageTest {
+public class RedirectPageTest1 {
 
     BrowserManager manager = new BrowserManager();
 
