592e42b31796f0b502d8ee4e1a1a764ce421a9c2
Marius <mariusmihalache08@gmail.com>
2025-01-29 19:23:41 +0200

    - 29.01.2025

--- src/main/java/org/automation/framework/constants/LoginDetails.java
+++ src/main/java/org/automation/framework/constants/LoginDetails.java
@@ -2,11 +2,9 @@ package org.automation.framework.constants;
 
 public enum LoginDetails {
 
-    LOGIN_USER("mariusmihalache@gmail.com", "Enesedetamina");
-//nu l am putut adauga
-     INVALID_USER("marius","123") {
-
-   }
+    LOGIN_USER("mariusmihalache08@gmail.com", "Enesedetamina179"),
+    INVALID_USER("User", "123786"),
+    EMPTY_USER("empty_USER");
 
     private final String username;
     private String password;
@@ -18,7 +16,6 @@ public enum LoginDetails {
 
     LoginDetails(String username) {
         this.username = username;
-
     }
 
     public String getUsername() {
--- src/main/java/org/automation/pageobjects/LoginFox.java
+++ src/main/java/org/automation/pageobjects/LoginFox.java
@@ -1,12 +1,12 @@
+
 package org.automation.pageobjects;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.automation.constants.LoginDetails;
 import org.automation.framework.BrowserManager;
 import org.automation.framework.SeleniumActions;
-import org.automation.framework.constants.LoginDetails;
 import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginFox {
 
@@ -16,26 +16,29 @@ public class LoginFox {
 
     private final static By USERNAME_FIELD = By.xpath("//*[@id=login_popup711]");
     private final static By PASSWORD_FIELD = By.xpath("//*[@id=psw_popup711]");
-    private final static By SUMIT_BUTTON = By.xpath("//*[@id=popup711_login_popup_form_container]/form/div[4]/div[1]/button");
-
+    private final static By SUBMIT_BUTTON = By.xpath("//*[@id=popup711_login_popup_form_container]/form/div[4]/div[1]/button");
+    //  private final static By ADMIN_SEARCH_BUTTON = By.xpath("//input[@placeholder='Search']");
+    //  private final static By ADMIN_SEARCH_RESULT = By.xpath("//li[@class='oxd-main-menu-item-wrapper']");
+    //  private final static By SEARCH_BOX_ADMIN_PAGE = By.xpath("//div[@class='oxd-input-group oxd-input-field-bottom-space']//input[@class='oxd-input oxd-input--active']");
+    //  private final static By SEARCH_BUTTON_ADMIN_PAGE = By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space']");
 
     public void openFoxLoginPage() {
         log.info("Open Fox login page");
         manager.openBrowser();
         manager.getDriver().get("https://www.foxfishing.ro/");
         manager.getDriver().manage().window().maximize();
-
-
     }
 
-    // a fost creata automat
-    public void FoxLoginPage() {
-        log.info("Login to Fox");
+    public void loginFox() {
+        log.info("Login to Fox Page");
+        String user = LoginDetails.LOGIN_USER.getUsername();
+        String pass = LoginDetails.LOGIN_USER.getPassword();
 
-        String user = LoginDetails.INVALID_USER.getUsername();
-        String pass = LoginDetails.INVALID_USER.getPassword();
-        actions.sendKeys(USERNAME_FIELD,user);
-        actions.sendKeys(PASSWORD_FIELD,pass);
-        actions.clickElement(SUMMIT_BUTTON);
+        actions.waitElementToBeClickable(USERNAME_FIELD, 10);
+        actions.sendKeys(USERNAME_FIELD, user);
+        actions.sendKeys(PASSWORD_FIELD, pass);
+        actions.clickElement(SUBMIT_BUTTON);
     }
 }
+
+
